a1ba6ea13b3b934061a9572d598f776f0acf06bf
HamzyP <134822658+HamzyP@users.noreply.github.com>
2026-08-31 17:16:22 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -671,7 +671,7 @@ int main(){
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
   std::set<std::string> shell_cmds = {
-    "echo", "type", "exit", "pwd", "cd", "complete" };
+    "echo", "type", "exit", "pwd", "cd", "complete", "jobs" };
 
   rl_attempted_completion_function = completion;
 
